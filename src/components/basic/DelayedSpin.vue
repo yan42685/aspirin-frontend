f989b5bdf3c795a0a5d51978ec4619f11ae52fcc
alexyan <419725794@qq.com>
2020-12-04 09:27:39 +0800

    feat: 添加全局加载中

---
+++ src/components/basic/DelayedSpin.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <a-spin :spinning="loading" :delay="delayTime">
+      <slot></slot>
+    </a-spin>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  name: "DelayedSpin",
+  props: {
+    // 正在加载中
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    // 延迟显示
+    delayTime: {
+      type: Number,
+      default: 500
+    }
+  },
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped>
+.spin-content {
+  border: 1px solid #91d5ff;
+  background-color: #e6f7ff;
+  padding: 30px;
+}
+</style>
--- src/views/student/PersonalCenter.vue
+++ src/views/student/PersonalCenter.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <a-card :loading="loading" title="Card title">
-      whatever content
-    </a-card>
-    <a-button style="marginTop: 16px" @click="handleClick">
-      Toggle loading
-    </a-button>
-  </div>
+  <delayed-spin>
+    <div class="card-container">
+      <a-card class="card-left">
+        whatever content
+      </a-card>
+      <a-card class="card-right">
+        whatever content
+      </a-card>
+    </div>
+  </delayed-spin>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs } from "vue";
+import DelayedSpin from "@/components/basic/DelayedSpin.vue";
 
 export default defineComponent({
+  components: { DelayedSpin },
   name: "PersonalCenter",
   setup() {
     const data = reactive({
@@ -26,4 +30,26 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.card-container {
+  padding: 1.5%;
+  display: flex;
+  justify-content: space-between;
+  .card-left,
+  .card-right {
+    min-height: 80vh;
+  }
+  .card-left {
+    width: 33.5%;
+    &:hover {
+      box-shadow: $base-card-box-shadow;
+    }
+  }
+  .card-right {
+    width: 63.5%;
+    &:hover {
+      box-shadow: $base-card-box-shadow;
+    }
+  }
+}
+</style>
