a1e6864814e375ce74065157a1ece1239e0b390b
alexyan <419725794@qq.com>
2020-12-06 00:33:26 +0800

    feat: 请求超时重试

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,16 +3,36 @@ import qs from "qs";
 import { ExceptionEnum, JsonWrapper } from "@/api/rest-api";
 import { message } from "ant-design-vue";
 import { messenger } from "./my-ant-design-vue";
+// axios重试插件
+import * as rax from "retry-axios";
 
 // 直接调用Axios可以处理多个请求, Axios.all([request1, ...]).then(() => Axios.spread(result1, ...) => {})
+
+// 请求超时的时限
+const REQUEST_TIMEOUT = 10000;
 
 // create an axios instance
 const httpClient = axios.create({
   // npm run serve 默认指向.env.development
   baseURL: process.env.VUE_APP_API_BASE_URL,
   withCredentials: true, // send cookies when cross-domain requests
-  timeout: 15000 // request timeout
+  timeout: REQUEST_TIMEOUT, // request timeout
+  raxConfig: {
+    // 重试次数
+    retry: 3,
+    onRetryAttempt: err => {
+      messenger.warning(`请求超时，正在重试`);
+    }
+  }
 });
+// 绑定实例 详见: https://github.com/JustinBeckwith/retry-axios/issues/121
+httpClient.defaults.raxConfig = {
+  ...httpClient.defaults.raxConfig,
+  instance: httpClient
+};
+
+// 使重试插件生效
+const interceptorId = rax.attach(httpClient);
 
 // 处理response异常
 function handleError<T>(result: JsonWrapper<T>): void {
@@ -55,11 +75,11 @@ httpClient.interceptors.response.use(
   error => {
     // 异常处理
 
-    const { code, message } = error;
-    if (code === "ECONNABORTED" || message === "Network Error") {
-      // 请求超时
-      messenger.error(`请求超时，网络状况不佳`);
-    }
+    /* const { code, message, config } = error;                      */
+    /* if (code === "ECONNABORTED" || message === "Network Error") { */
+    /*   // 请求超时                                             */
+    /*   messenger.error(`请求超时，网络状况不佳`);       */
+    /* }                                                             */
 
     // 可以进行相关提示等处理
     return Promise.reject(error);
