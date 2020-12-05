1660780ea11211964ebae0b842e9776dde19faea
alexyan <419725794@qq.com>
2020-12-05 14:50:39 +0800

    feat: 实现全屏按钮

--- src/layout/content/UseContent.vue
+++ src/layout/content/UseContent.vue
@@ -42,7 +42,6 @@ export default defineComponent({
             }
           }
         });
-        console.log(cachedTabNames);
         return cachedTabNames;
       })
     });
--- src/layout/header/UseHeader.vue
+++ src/layout/header/UseHeader.vue
@@ -4,10 +4,12 @@
       <a-row class="sub-header1">
         <!-- NOTE: 栅格系统是指针对 100vw进行划分宽度，由于Framework只给Header分配了21列，所以这里总和应该是21列 -->
         <a-col :span="12"><use-bread-crumb /></a-col>
-        <a-col :span="9" class="sub-header1-right"
-          ><a-button shape="circle" @click.stop="onReloadClick">
-            <template #icon><RedoOutlined /> </template></a-button
-        ></a-col>
+        <a-col :span="9" class="sub-header1-right">
+          <full-screen-button />
+          <a-button shape="circle" @click.stop="reloadTab">
+            <template #icon><RedoOutlined /> </template>
+          </a-button>
+        </a-col>
       </a-row>
       <div class="sub-header2"><use-tab-bar /></div>
     </a-layout-header>
@@ -15,19 +17,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, reactive, toRefs, computed } from "vue";
 import UseBreadCrumb from "./component/UseBreadCrumb.vue";
 import UseTabBar from "./component/UseTabBar.vue";
 import { messenger } from "@/utils/my-ant-design-vue";
 import { eventBus } from "@/utils/event-bus";
 import { RedoOutlined } from "@ant-design/icons-vue";
+import FullScreenButton from "@/components/button/FullScreenButton.vue";
 
 export default defineComponent({
-  components: { UseBreadCrumb, UseTabBar, RedoOutlined },
+  components: {
+    UseBreadCrumb,
+    UseTabBar,
+    RedoOutlined,
+    FullScreenButton
+  },
   name: "UseHeader",
   setup() {
     const data = reactive({
-      onReloadClick: () => eventBus.emit("reloadTab")
+      reloadTab: () => eventBus.emit("reloadTab")
     });
     return { ...toRefs(data) };
   }
@@ -48,8 +56,11 @@ export default defineComponent({
 
   .sub-header1 {
     &-right {
-      justify-content: left;
       align-items: center;
+
+      .ant-btn {
+        margin: 0 5px;
+      }
     }
   }
 
