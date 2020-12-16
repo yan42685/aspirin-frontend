9ede0101f2a532a172eee5b938d5bf7a435be068
alexyan <419725794@qq.com>
2020-12-16 20:11:07 +0800

    refactor

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import { internalConfig } from "@/config/app-settings";
 import { store } from "@/store";
 import { messenger } from "@/utils/my-ant-design-vue";
-import { hasPermission } from "@/utils/route";
+import { hasPermission, addDynamicRoutes } from "@/utils/route";
 import { loginRedirect, homeRedirect } from "@/utils/timeout-actions";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Framework from "../layout/framework/Framework.vue";
 import { studentRoutes } from "./modules/student";
 import { teacherRoutes } from "./modules/teacher";
+import { cookies } from "@/utils/basic-lib";
 
 // 旧版本是RouteConfig 新版本是RouteRecordRaw
 export const staticRootRoutes: Array<RouteRecordRaw> = [
@@ -117,7 +118,7 @@ const ROUTE_WHITE_LIST = ["/login", "/register", "/404"];
 router.beforeEach(async (to, from, next) => {
   if (ROUTE_WHITE_LIST.includes(to.path)) {
     next();
-  } else if (internalConfig.loginInterception && !store.state.user.role) {
+  } else if (internalConfig.loginInterception && !cookies.get("aspirin-role")) {
     loginRedirect();
   } else if (internalConfig.accessControl && !hasPermission(to)) {
     messenger.warning("抱歉，您没有权限访问此资源");
@@ -133,5 +134,7 @@ router.afterEach(to => {
     document.title = to.meta.title + " - " + internalConfig.appName;
   }
 });
+
+addDynamicRoutes();
 
 export { router };
