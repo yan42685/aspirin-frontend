619c8b1023deb0a3a49845d73c0c87c8ee21006a
alexyan <419725794@qq.com>
2021-01-13 19:38:06 +0800

    影厅界面

--- src/views/movie/MovieHall.vue
+++ src/views/movie/MovieHall.vue
@@ -1,5 +1,12 @@
 <template>
   <white-background :loading="loading">
+    <h1>影厅列表</h1>
+    <a-modal :title="modalTitle" v-model:visible="modalVisible" @ok="modalOk">
+      <div class="form">
+        <a-label>影厅名称</a-label>
+        <a-input v-model:value="currentHall.name" />
+      </div>
+    </a-modal>
     <div v-if="!loading">
       <!-- scroll可以固定table的长度，然后内部滑动 -->
       <a-table
@@ -10,10 +17,9 @@
         :scroll="{ y: 400 }"
       >
         <template #action="{ record }">
-          <elect-course-button
-            :courseDetailId="record.id"
-            :courseStatus="record.status"
-          />
+          <a @click="modifyHall(record)">修改</a>&nbsp;
+          <a @click="addHall()">添加</a>&nbsp;
+          <a @click="deleteHall(record.id)">删除</a>
         </template>
       </a-table>
     </div>
@@ -23,9 +29,16 @@
 <script lang="ts">
 import { Hall } from "@/api/movie-rest-api";
 import { messenger } from "@/utils/my-ant-design-vue";
-import { getRequest } from "@/utils/request";
-import { defineComponent, reactive, toRefs } from "vue";
+import {
+  deleteRequest,
+  getRequest,
+  postRequest,
+  putRequest,
+} from "@/utils/request";
+import { computed, defineComponent, reactive, ref, toRefs } from "vue";
 import WhiteBackground from "@/components/basic/WhiteBackground";
+
+type Action = "add" | "modify";
 
 export default defineComponent({
   name: "MovieHall",
@@ -34,16 +47,82 @@ export default defineComponent({
   setup() {
     const data = reactive({
       loading: false,
-      chooseDisabled: true,
+      modalAction: "modify" as Action,
+      modalVisible: false,
+      modalTitle: "",
       columns: [
         { title: "影厅名称", dataIndex: "name" },
         {
           title: "操作",
           key: "action",
+          slots: { customRender: "action" },
           width: 260,
         },
       ],
       hallList: [] as Hall[],
+
+      // modal里保存的hall信息
+      currentHall: {} as Hall,
+      rowList: [[0]] as number[][],
+    });
+
+    const methods = reactive({
+      modalOk() {
+        if (data.modalAction === "add") {
+          postRequest("/hall", data.currentHall).then((res) => {
+            if (res.code === 0) {
+              const list = res.data as Hall[];
+              data.hallList = list;
+              messenger.success("添加影厅成功");
+            } else {
+              messenger.error("添加影厅失败");
+            }
+          });
+        } else if (data.modalAction === "modify") {
+          putRequest("/hall", data.currentHall).then((res) => {
+            if (res.code === 0) {
+              const list = res.data as Hall[];
+              data.hallList = list;
+              messenger.success("修改影厅成功");
+            } else {
+              messenger.error("修改影厅失败");
+            }
+          });
+        }
+        data.modalVisible = false;
+      },
+      modifyHall(record: Hall) {
+        data.modalTitle = "修改";
+        data.modalAction = "modify";
+        data.modalVisible = true;
+        data.currentHall = record;
+        // TODO: 处理解析错误的情况
+        data.rowList = JSON.parse(record.seats);
+      },
+      addHall() {
+        data.modalTitle = "添加";
+        data.modalAction = "add";
+        data.modalVisible = true;
+        data.currentHall = {} as Hall;
+        data.rowList = [[0]];
+      },
+      deleteHall(id: number) {
+        deleteRequest("/hall", { id: id }).then((res) => {
+          if (res.code === 0) {
+            const list = res.data as Hall[];
+            data.hallList = list;
+          } else {
+            messenger.error("删除影厅失败");
+          }
+        });
+      },
+
+      addRow() {
+        data.rowList.push([0]);
+      },
+      deleteRow(index: number) {
+        data.rowList.splice(index, 1);
+      },
     });
 
     async function fetchInfo() {
@@ -59,7 +138,7 @@ export default defineComponent({
 
     fetchInfo();
 
-    return { ...toRefs(data) };
+    return { ...toRefs(data), ...toRefs(methods) };
   },
 });
 </script>
---
+++ src/views/movie/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "test",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
