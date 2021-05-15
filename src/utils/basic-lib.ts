80442015d3b9a67d65283a7afefb0e5457f730a1
alexyan <419725794@qq.com>
2021-05-15 14:07:02 +0800

    完成随机数乘法与减法

--- src/utils/basic-lib.ts
+++ src/utils/basic-lib.ts
@@ -4,162 +4,167 @@ import Cookies from "js-cookie";
 
 // 获取随机字符串
 export function randomString(length = 20) {
-  const charset =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let result = "";
-  for (let i = length; i > 0; --i)
-    result += charset[Math.floor(Math.random() * charset.length)];
-  return result;
+    const charset =
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    for (let i = length; i > 0; --i)
+        result += charset[Math.floor(Math.random() * charset.length)];
+    return result;
 }
 
 // setInterval之后立即执行一次
 export function setIntervalImmediately(func: Function, interval: number) {
-  func();
-  return setInterval(func, interval);
+    func();
+    return setInterval(func, interval);
 }
 
 // 平整状态
 export function array2Object<T extends { id: string }>(
-  array: Array<T>
+    array: Array<T>
 ): { [key: string]: T } {
-  return array.reduce((prev, cur) => {
-    if (cur.id) {
-      prev[cur.id] = cur;
-    }
-    return prev;
-  }, {} as { [key: string]: T });
+    return array.reduce((prev, cur) => {
+        if (cur.id) {
+            prev[cur.id] = cur;
+        }
+        return prev;
+    }, {} as { [key: string]: T });
 }
 
 export function object2Array<T>(obj: { [key: string]: T }): Array<T> {
-  return Object.keys(obj).map(key => obj[key]);
+    return Object.keys(obj).map((key) => obj[key]);
 }
 
 export function isClickOutside(elementRef: Ref<null | HTMLElement>) {
-  const isClickOutside = ref(false);
+    const isClickOutside = ref(false);
 
-  const handler = (event: MouseEvent) => {
-    if (
-      elementRef.value &&
-      elementRef.value.contains(event.target as HTMLElement)
-    ) {
-      isClickOutside.value = true;
-    } else {
-      isClickOutside.value = false;
-    }
-  };
+    const handler = (event: MouseEvent) => {
+        if (
+            elementRef.value &&
+            elementRef.value.contains(event.target as HTMLElement)
+        ) {
+            isClickOutside.value = true;
+        } else {
+            isClickOutside.value = false;
+        }
+    };
 
-  onMounted(() => {
-    document.addEventListener("click", handler);
-  });
-  onUnmounted(() => {
-    document.removeEventListener("click", handler);
-  });
-  return isClickOutside;
+    onMounted(() => {
+        document.addEventListener("click", handler);
+    });
+    onUnmounted(() => {
+        document.removeEventListener("click", handler);
+    });
+    return isClickOutside;
 }
 
 export function concatPath(base: string, current: string) {
-  let result = "";
-  if (current === "/") {
-    result = "/";
-  } else if (base === "" || base === "/") {
-    result = "/" + current;
-  } else {
-    result = base.trim() + "/" + current.trim();
-  }
-  // HACK:
-  if (result.startsWith("//")) {
-    result = result.slice(1);
-  }
+    let result = "";
+    if (current === "/") {
+        result = "/";
+    } else if (base === "" || base === "/") {
+        result = "/" + current;
+    } else {
+        result = base.trim() + "/" + current.trim();
+    }
+    // HACK:
+    if (result.startsWith("//")) {
+        result = result.slice(1);
+    }
 
-  return result;
+    return result;
 }
 
 export class AutoRetryConfig {
-  // 重试计数器
-  retryCount = 0;
-  maxRetryCount = 30;
-  retryDelay = 500;
+    // 重试计数器
+    retryCount = 0;
+    maxRetryCount = 30;
+    retryDelay = 500;
 }
 
 // fn 需要是一个失败return false, 成功返回true的函数
 // HACK: 失败自动重复的通用函数
 export function autoRetry(
-  fn: () => boolean,
-  config: AutoRetryConfig = new AutoRetryConfig()
+    fn: () => boolean,
+    config: AutoRetryConfig = new AutoRetryConfig()
 ) {
-  const retryCount = config.retryCount;
-  if (retryCount > config.maxRetryCount) {
-    // 表示最终重试失败
-    return false;
-  }
-  if (!fn()) {
-    // console.log(`第${callSelfCount + 1}次调用失败`);
-    setTimeout(
-      () => autoRetry(fn, { ...config, retryCount: retryCount + 1 }),
-      config.retryDelay
-    );
-    return false;
-  }
-  // console.log(`第${callSelfCount + 1}次调用成功`);
-  return true;
+    const retryCount = config.retryCount;
+    if (retryCount > config.maxRetryCount) {
+        // 表示最终重试失败
+        return false;
+    }
+    if (!fn()) {
+        // console.log(`第${callSelfCount + 1}次调用失败`);
+        setTimeout(
+            () => autoRetry(fn, { ...config, retryCount: retryCount + 1 }),
+            config.retryDelay
+        );
+        return false;
+    }
+    // console.log(`第${callSelfCount + 1}次调用成功`);
+    return true;
 }
 
 export async function autoRetryAsync(
-  fn: () => Promise<boolean>,
-  config: AutoRetryConfig = new AutoRetryConfig()
+    fn: () => Promise<boolean>,
+    config: AutoRetryConfig = new AutoRetryConfig()
 ) {
-  const retryCount = config.retryCount;
-  if (retryCount > config.maxRetryCount) {
-    return false;
-  }
+    const retryCount = config.retryCount;
+    if (retryCount > config.maxRetryCount) {
+        return false;
+    }
 
-  const isSuccess = await fn();
-  if (!isSuccess) {
-    setTimeout(
-      () => autoRetryAsync(fn, { ...config, retryCount: retryCount + 1 }),
-      config.retryDelay
-    );
-    return false;
-  }
-  return true;
+    const isSuccess = await fn();
+    if (!isSuccess) {
+        setTimeout(
+            () => autoRetryAsync(fn, { ...config, retryCount: retryCount + 1 }),
+            config.retryDelay
+        );
+        return false;
+    }
+    return true;
 }
 
 export async function autoRetryUtilFetchedUserInfo(
-  fn: () => void,
-  config: AutoRetryConfig = new AutoRetryConfig()
+    fn: () => void,
+    config: AutoRetryConfig = new AutoRetryConfig()
 ) {
-  const fnWrapper = async () => {
-    if (!store.state.user.isInfoFetched) {
-      return false;
-    } else {
-      await fn();
-      return true;
-    }
-  };
-  autoRetryAsync(fnWrapper);
+    const fnWrapper = async () => {
+        if (!store.state.user.isInfoFetched) {
+            return false;
+        } else {
+            await fn();
+            return true;
+        }
+    };
+    autoRetryAsync(fnWrapper);
 }
 
 export async function autoRetryUtilFetchedTeacherInfo(
-  fn: () => void,
-  config: AutoRetryConfig = new AutoRetryConfig()
+    fn: () => void,
+    config: AutoRetryConfig = new AutoRetryConfig()
 ) {
-  const fnWrapper = async () => {
-    if (!store.state.teacher.info.username) {
-      return false;
-    } else {
-      fn();
-      return true;
-    }
-  };
-  autoRetryAsync(fnWrapper);
+    const fnWrapper = async () => {
+        if (!store.state.teacher.info.username) {
+            return false;
+        } else {
+            fn();
+            return true;
+        }
+    };
+    autoRetryAsync(fnWrapper);
 }
 
 export function isStrEmpty(str: string) {
-  return !str || 0 === str.length;
+    return !str || 0 === str.length;
 }
 
 export function isStrBlank(str: string) {
-  return !str || /^\s*$/.test(str);
+    return !str || /^\s*$/.test(str);
+}
+
+// 生成 [min, max) 随机数
+export function randomNum(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export const cookies = Cookies;
--- src/views/pages/OralCalculation.vue
+++ src/views/pages/OralCalculation.vue
@@ -1,8 +1,19 @@
 <template>
   <white-background>
-    <div>
-      <ul>
-        <li v-for="n in 10" :key="n">{{ n }}</li>
+    <div class="multiplication">
+      <ul class="problem-area">
+        <li v-for="i in multiplicationCount" :key="i">
+          {{ factorAs[i - 1] }} x {{ factorBs[i - 1] }} =
+          {{ productions[i - 1] }}
+        </li>
+      </ul>
+    </div>
+    <div class="subtraction">
+      <ul class="problem-area">
+        <li v-for="i in subtractionCount" :key="i">
+          {{ minuends[i - 1] }} - {{ subtrahends[i - 1] }} =
+          {{ differences[i - 1] }}
+        </li>
       </ul>
     </div>
   </white-background>
@@ -10,15 +21,56 @@
 
 <script lang="ts">
 import WhiteBackground from "@/components/basic/WhiteBackground.vue";
-import { defineComponent } from "vue";
+import { computed, defineComponent, reactive, toRefs } from "vue";
+import { randomNum } from "@/utils/basic-lib";
 
 export default defineComponent({
   components: { WhiteBackground },
   name: "OralCalculation",
   setup() {
-    return {};
+    const data = reactive({
+      multiplicationCount: 15,
+      subtractionCount: 15,
+      factorAs: [] as number[],
+      factorBs: [] as number[],
+      productions: [] as number[],
+      minuends: [] as number[],
+      subtrahends: [] as number[],
+      differences: [] as number[],
+    });
+
+    function fillNumbers() {
+      for (let i = 0; i < data.multiplicationCount; i++) {
+        data.factorAs[i] = randomNum(11, 100);
+        data.factorBs[i] = randomNum(11, 100);
+        data.productions[i] = data.factorAs[i] * data.factorBs[i];
+      }
+
+      for (let i = 0; i < data.subtractionCount; i++) {
+        data.minuends[i] = randomNum(1001, 10000);
+        data.subtrahends[i] = randomNum(1001, 10000);
+        data.differences[i] = data.minuends[i] - data.subtrahends[i];
+      }
+    }
+
+    fillNumbers();
+
+    return { ...toRefs(data) };
   },
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.problem-area {
+  //   height: 500px;
+  //   width: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    display: inline-block;
+    margin: 10px;
+    width: 150px;
+    height: 30px;
+  }
+}
+</style>
